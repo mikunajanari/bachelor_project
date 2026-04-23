f7773590ee46bac834a3093208c378bc470aad05
Daria Bryzhyniuk <daria@email.com>
2026-04-23 22:40:29 +0300

    Made TabPanelDown

---
+++ cat_game/Assets/Scripts/Utilites/ScriptableObjects/SoundEffect.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace cat_game
+{
+    public class SoundEffect : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
